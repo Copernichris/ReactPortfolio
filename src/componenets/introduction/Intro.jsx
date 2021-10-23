a59549ce5b37adec5dddaf6e3b8163df673171f4
copernichris <christhomas2101@gmail.com>
2021-10-23 19:31:58 -0400

    pages fix

--- src/componenets/introduction/Intro.jsx
+++ src/componenets/introduction/Intro.jsx
@@ -11,7 +11,7 @@ export default function Intro() {
             <div className="right">
                 <div className="wrapper">
                     <p>Hello! My name is</p>
-                    <h1>Chris Thomas</h1>
+                    <h1>Christopher Thomas</h1>
                     <p>I am a web developer looking for a full time position</p>
                 </div>
             </div>
